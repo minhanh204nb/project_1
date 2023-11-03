70c256eadea1bfd33f0f6b319e61e31bfc146413
minhanh204nb <minhanh24hihi@gmail.com>
2023-11-04 03:57:33 +0700

    main

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # du_an_1_team_10
 project 1 team 10 gồm 3 thành viên cao đẳng 
+#website tham khao 
+https://www.lottecinemavn.com/LCHS/index.aspx
+https://www.galaxycine.vn/
+# bao cao tham khao
+https://docs.google.com/document/d/1b0wfxctvcoBtoG1xa0frcf4DVhSiCnyW7TLkFp6b5LM/edit#heading=h.asjrpk8gfalz
---
+++ layout/navbar.php
@@ -0,0 +1,85 @@
+<header class="header" data-header>
+    <div class="container">
+        <div class="overlay" data-overlay></div>
+        <a href="./index.html" class="logo">
+            <img src="./assets/images/logo.svg" alt="Filmlane logo" />
+        </a>
+        <form class="header-actions">
+            <div>
+                <input type="text" name="search" />
+            </div>
+            <button class="search-btn">
+                <ion-icon name="search-outline"></ion-icon>
+            </button>
+            <div class="lang-wrapper">
+                <label for="language">
+                    <ion-icon name="globe-outline"></ion-icon>
+                </label>
+                <select name="language" id="language">
+                    <option value="en">EN</option>
+                    <option value="au">AU</option>
+                    <option value="ar">AR</option>
+                    <option value="tu">TU</option>
+                </select>
+            </div>
+            <button class="btn btn-primary">Sign in</button>
+        </form>
+        <button class="menu-open-btn" data-menu-open-btn>
+            <ion-icon name="reorder-two"></ion-icon>
+        </button>
+        <nav class="navbar" data-navbar>
+            <div class="navbar-top">
+                <a href="./index.html" class="logo">
+                    <img src="./assets/images/logo.svg" alt="Filmlane logo" />
+                </a>
+                <button class="menu-close-btn" data-menu-close-btn>
+                    <ion-icon name="close-outline"></ion-icon>
+                </button>
+            </div>
+            <ul class="navbar-list">
+                <li>
+                    <a href="./index.html" class="navbar-link">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-link">Movie</a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-link">Tv Show</a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-link">Web Series</a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-link">Pricing</a>
+                </li>
+            </ul>
+            <ul class="navbar-social-list">
+                <li>
+                    <a href="#" class="navbar-social-link">
+                        <ion-icon name="logo-twitter"></ion-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-social-link">
+                        <ion-icon name="logo-facebook"></ion-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-social-link">
+                        <ion-icon name="logo-pinterest"></ion-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-social-link">
+                        <ion-icon name="logo-instagram"></ion-icon>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-social-link">
+                        <ion-icon name="logo-youtube"></ion-icon>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
