37688baf67caa6377edfcb52a87c5173e325e619
minhanh204nb <minhanh24hihi@gmail.com>
2023-11-04 16:19:32 +0700

    main

---
+++ admin/index.php
@@ -0,0 +1,5 @@
+<?php
+include './models/pdo.php';
+include './layouts/head.php';
+include './layouts/navbar.php';
+include './layouts/sidebar.php';
---
+++ admin/layouts/sidebar.php
@@ -0,0 +1,45 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title"><span>Main</span></li>
+                <li class="active">
+                    <a href=""><i data-feather="home"></i> <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="users"></i> <span>Customers</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="file-text"></i> <span>Estimates</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="star"></i> <span>Items</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="credit-card"></i> <span>Payments</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="package"></i> <span>Expenses</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="settings"></i> <span>Settings</span></a>
+                </li>
+                <li class="menu-title">
+                    <span>Pages</span>
+                </li>
+                <li>
+                    <a href=""><i data-feather="user-plus"></i> <span>Profile</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="user"></i> <span>Users</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="file"></i> <span>Blank Page</span></a>
+                </li>
+                <li>
+                    <a href=""><i data-feather="map-pin"></i> <span>Vector Maps</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
