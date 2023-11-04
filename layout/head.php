257d90d327ef646e1a6baa1fadd14449835f678f
minhanh204nb <minhanh24hihi@gmail.com>
2023-11-05 03:01:03 +0700

    main

--- layout/head.php
+++ layout/head.php
@@ -9,3 +9,30 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
 </head>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css" />
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.min.css" />
+    <link rel="stylesheet" href="assets/css/nouislider.min.css" />
+    <link rel="stylesheet" href="assets/css/ionicons.min.css" />
+    <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+    <link rel="stylesheet" href="assets/css/plyr.css" />
+    <link rel="stylesheet" href="assets/css/photoswipe.css" />
+    <link rel="stylesheet" href="assets/css/default-skin.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" href="assets/icon/favicon-32x32.png" sizes="32x32" />
+    <link rel="apple-touch-icon" href="assets/icon/favicon-32x32.png" />
+
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Dmitry Volkov" />
+    <title>HotFlix – Online Movies, TV Shows & Cinema HTML Template</title>
+</head>
