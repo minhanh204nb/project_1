481217553ec96ae4d261ba9e080e31398fdf1cd5
minhanh204nb <minhanh24hihi@gmail.com>
2023-11-04 17:15:23 +0700

    main

--- admin/index.php
+++ admin/index.php
@@ -3,3 +3,19 @@ include './models/pdo.php';
 include './layouts/head.php';
 include './layouts/navbar.php';
 include './layouts/sidebar.php';
+
+
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    switch ($action) {
+        case 'create':
+            break;
+        case 'update':
+            break;
+        default:
+            break;
+    }
+} else {
+    include './views/total_list.php';
+    include './views/analytics.php';
+}
