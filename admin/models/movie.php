00e630543fbfdc18dd8c71f32a141050dc23483f
minhanh204nb <minhanh24hihi@gmail.com>
2023-11-14 05:04:57 +0700

    update

--- admin/models/movie.php
+++ admin/models/movie.php
@@ -1,11 +1,17 @@
 <?php
-function insert_movie($name_movie, $content, $id_country, $year, $time, $riviews, $author, $performer, $age_limit, $image, $trailer_movie, $id_category, $action)
+function insert_movie($name_movie, $content, $id_country, $year, $time, $reviews, $author, $performer, $age_limit, $image, $trailer_movie, $id_category, $action)
 {
-    $sql = "INSERT INTO movie (name_movie, content, id_country, year, time, rviews, author, performer, age_limit, image, trailer_movie, id_category, action) VALUES ('$name_movie', '$content', '$id_country','$year', '$time', '$riviews', '$author', '$performer', '$age_limit', '$image','$trailer_movie', '$id_category', '$action')";
+    $sql = "INSERT INTO movie (name_movie, content, id_country, year, time, reviews, author, performer, age_limit, image, trailer_movie, id_category, action) VALUES ('$name_movie', '$content', '$id_country','$year', '$time', '$reviews', '$author', '$performer', '$age_limit', '$image','$trailer_movie', '$id_category', '$action')";
     pdo_execute($sql);
 }
 function delete_movie($id_movie)
 {
     $sql = "DELETE FROM movie WHERE id_movie = '$id_movie'";
     pdo_execute($sql);
+}
+function loadall_movie()
+{
+    $sql = "SELECT * FROM movie order by id_movie desc";
+    $list_movie = pdo_query($sql);
+    return $list_movie;
 }
